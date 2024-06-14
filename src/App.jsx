addc3df42cf9f37bd7bc6730a5fef5932c3b8961
cesar angulo <cesarangulo@cesars-Mac-Studio.local>
2024-06-13 21:07:57 -0700

    hello plebes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Page from "./app/page";
+
+
+function App() {
+  return (
+    <>
+      <Page></Page>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { AuthProvider } from "./AuthPlug.jsx";
+import { Buffer } from "buffer/";
+import { MetaMaskProvider } from "metamask-react";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+
+globalThis.Buffer = Buffer;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <MetaMaskProvider>
+          <App />
+        </MetaMaskProvider>
+      </BrowserRouter>
+    </AuthProvider>
+    ,
+  </React.StrictMode>
+);
