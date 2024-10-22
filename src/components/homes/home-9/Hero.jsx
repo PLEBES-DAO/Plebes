7dce82467dc0e6c32a6eeecf63cbc857decd76c8
AbelCodes <abelg896@gmail.com>
2024-10-22 16:50:08 +0000

    frontend home changes two

--- src/components/homes/home-9/Hero.jsx
+++ src/components/homes/home-9/Hero.jsx
@@ -30,7 +30,7 @@ export default function Hero() {
                 to="/collections"
                 className="rounded-full bg-accent py-3 px-8 text-center font-semibold text-white shadow-accent-volume transition-all hover:bg-accent-dark"
               >
-                white paper
+                White paper
               </Link>
             </div>
           </div>
