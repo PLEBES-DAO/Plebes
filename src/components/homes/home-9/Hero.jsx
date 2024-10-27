1639f38009fb7d720c3bffa29329177367a7d1a7
cesar angulo <cesarangulo@cesars-MacBook-Air.local>
2024-10-27 14:28:59 -0700

    visual changes

--- src/components/homes/home-9/Hero.jsx
+++ src/components/homes/home-9/Hero.jsx
@@ -1,42 +1,33 @@
 import { useBioniqContext } from '../../../hooks/BioniqContext'
-import Image from '../../common/Image'
 import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
 
 export default function Hero() {
-  const navigate = useNavigate();
-  const{createAuction,cancelAuction}=useBioniqContext()
+  const { createAuction, cancelAuction } = useBioniqContext()
+
   return (
-    <section className="relative h-screen sm:h-full">
-      <picture className="pointer-events-none absolute -z-10 inset-0">
-        <Image
-          width={1920}
-          height={1014}
-          src="/img/background.png"
-          alt="image"
-          className="absolute inset-0 -z-10 h-full w-full object-cover"
-        />
-      </picture>
-      <div className="ml-auto mr-auto h-full max-w-[91rem] px-4 sm:h-4">
+    <section className="relative h-screen">
+      <div
+        className="absolute inset-0 -z-10 h-full w-full bg-cover bg-center"
+        style={{ backgroundImage: "url('/img/background.png')" }}
+      />
+      <div className="ml-auto mr-auto h-full max-w-[91rem] px-4">
         <div className="grid h-full items-center gap-4 md:grid-cols-12">
           <div className="col-span-5 flex h-full flex-col items-center justify-center py-10 md:items-start lg:py-20">
             <h1 className="mb-6 text-center font-display text-5xl text-white md:text-left lg:text-6xl">
               Chihuahuas and frens for Public Goods*
             </h1>
             <p className="mb-8 max-w-md text-center text-lg text-white md:text-left">
-            Plebes is a DAO that promotes the use of Internet Computer Protocol by funding open-source technology, creative projects, and public goods through daily Reserved Ordinals auctions.
+              Plebes is a DAO that promotes the use of Internet Computer Protocol by funding open-source technology, creative projects, and public goods through daily Reserved Ordinals auctions.
             </p>
-            <div onClick={async () =>{navigate("/auction")}}  className="flex space-x-4">
-              <Link
-                href="/auctiond"
+            <div className='flex gap-3'>
+              <a
+                href="http://plebes.xyz/auction"
                 className="rounded-full bg-accent py-3 px-8 text-center font-semibold text-white shadow-accent-volume transition-all hover:bg-accent-dark"
               >
-                auction
-              </Link>
-            </div>
-            <div onClick={async ()=>{await cancelAuction()}} className="flex space-x-4">
+                Auction
+              </a>
               <Link
-                href="/collections"
+                to="/collections"
                 className="rounded-full bg-accent py-3 px-8 text-center font-semibold text-white shadow-accent-volume transition-all hover:bg-accent-dark"
               >
                 white paper
