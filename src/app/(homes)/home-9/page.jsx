0dd8798f99ad56f0acc4764a86aa36bcefeb60f3
plebesdev <plebesdev@gmail.com>
2025-04-13 23:04:21 -0600

    Changes to make this shit work

--- src/app/(homes)/home-9/page.jsx
+++ src/app/(homes)/home-9/page.jsx
@@ -1,10 +1,5 @@
-import Footer1 from "../../../components/footer/Footer1";
 import Navbar from "../../../components/headers/Navbar.jsx";
-import Benefits from "../../../components/homes/home-9/Benefits";
-import Faq from "../../../components/homes/home-9/Faq";
-import CoverFlowSlider from "../../../components/homes/home-4/CoverFlowSlider";
 import Hero from "../../../components/homes/home-9/Hero";
-import Intro from "../../../components/homes/home-9/Intro";
 
 export const metadata = {
 };
@@ -14,12 +9,8 @@ export default function HomePage9({ login, setModalOpen }) {
       <Navbar bLogin={login} setModalOpen={setModalOpen} />
       <main>
         <Hero />
-        <Intro />
-        <Benefits />
-         <CoverFlowSlider/> 
-        <Faq />
+        
       </main>
-      <Footer1 />
     </>
   );
 }
